c3a27a33d7787a2ed7038e34ac3119e2142f15f9
Jonas Simon Fleck <jonas.simon.fleck@embl.de>
2018-07-31 10:14:38 +0200

    Changes path joining

--- orion.py
+++ orion.py
@@ -25,9 +25,9 @@ from orion.interface import main_interface
 from orion.preprocessing import compute_features
 
 # CONST
-SCRIPT_DIR = os.path.abspath(os.path.dirname(sys.argv[0])) + "/"
+SCRIPT_DIR = os.path.abspath(os.path.dirname(sys.argv[0]))
 PFAM = os.environ["PFAM"]
-MODEL = SCRIPT_DIR + "data/model/f5_eval_p_t50.crf.model"
+MODEL = os.path.join(SCRIPT_DIR, "data/model/f5_eval_p_t50.crf.model")
 C1 = 0.15
 C2 = 1.75
 
@@ -44,8 +44,6 @@ if __name__ == "__main__":
     base = os.path.basename(fasta).split(".")[0]
 
     out_dir = args.out
-    if not out_dir.endswith("/"):
-        out_dir += "/"
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
 
@@ -56,14 +54,14 @@ if __name__ == "__main__":
 
     print("Running ORF prediction using PRODIGAL...")
 
-    prodigal_out = out_dir + "prodigal/"
+    prodigal_out = os.path.join(out_dir, "prodigal/")
     if not os.path.exists(prodigal_out):
         os.mkdir(prodigal_out)
 
-    coords_out = prodigal_out + base + ".coords.sco"
-    genes_out = prodigal_out + base + ".genes.fna"
-    proteins_out = prodigal_out + base + ".proteins.faa"
-    log_out = prodigal_out + ".prodigal.log"
+    coords_out = os.path.join(prodigal_out, base + ".coords.sco")
+    genes_out = os.path.join(prodigal_out, base + ".genes.fna")
+    proteins_out = os.path.join(prodigal_out, base + ".proteins.faa")
+    log_out = os.path.join(prodigal_out, base + ".prodigal.log")
     subprocess.run(["prodigal",
         "-i", fasta,
         "-o", coords_out,
@@ -85,18 +83,18 @@ if __name__ == "__main__":
 
     print(gene_order)
 
-    hmmer_out = out_dir + "hmmer/"
+    hmmer_out = os.path.join(out_dir, "hmmer/")
     if not os.path.exists(hmmer_out):
         os.mkdir(hmmer_out)
 
-    dom_out = hmmer_out + base + ".hmmer.dom"
-    log_out = hmmer_out + ".hmmer.log"
+    dom_out = os.path.join(hmmer_out, base + ".hmmer.dom")
+    log_out = os.path.join(hmmer_out, base + ".hmmer.log")
     subprocess.run(["hmmsearch",
         "--domtblout", dom_out, PFAM, proteins_out],
         stdout = open(log_out, "wt"),
         stderr = open(log_out, "wt"))
 
-    tsv_out = hmmer_out + base + ".hmmer.tsv"
+    tsv_out = os.path.join(hmmer_out, base + ".hmmer.tsv")
     convert_hmmer(dom_out, tsv_out)
 
     print("DONE")
@@ -108,7 +106,7 @@ if __name__ == "__main__":
     pfam_df = compute_features(pfam_df, weight_type=weight_type)
 
     # Write feature table to file
-    feat_out = out_dir + base + ".features.tsv"
+    feat_out = os.path.join(out_dir, base + ".features.tsv")
     pfam_df.to_csv(feat_out, sep="\t", index=False)
 
 
@@ -134,5 +132,5 @@ if __name__ == "__main__":
     pred_df = crf.predict_marginals()
 
     # Write predictions to file
-    pred_df = out_dir + base + ".pred.tsv"
+    pred_df = os.path.join(out_dir, base + ".pred.tsv")
     pfam_df.to_csv(pred_df, sep="\t", index=False)
