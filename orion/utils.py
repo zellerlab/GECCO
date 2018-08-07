85d05b732cab98e3c5ed4e67de0cdbe4baf4b9c4
Jonas Simon Fleck <jonas.simon.fleck@embl.de>
2018-08-07 10:35:50 +0200

    Added jsd()

--- orion/utils.py
+++ orion/utils.py
@@ -23,7 +23,7 @@ def coerce_numeric(s):
     except ValueError:
         return s
 
-def jsd(mat, base=np.e):
+def jsd(mat, base=2):
     """
     Computes Janson-Shannon Divergence given a matrix with probability vectors.
     """
@@ -39,3 +39,15 @@ def jsd(mat, base=np.e):
         dist_vec.append(dist)
 
     return np.array(dist_vec).reshape(len(mat), len(mat))
+
+def jsd_pairwise(p, q, base=2):
+    """
+    Computes Janson-Shannon Divergence given two probability vectors p and q.
+    """
+    p, q = np.asarray(p), np.asarray(q)
+
+    # Normalize p, q to probabilities
+    p, q = p/p.sum(), q/q.sum()
+    m = 1./2*(p + q)
+
+    return entropy(p, m, base=base)/2. + entropy(q, m, base=base)/2.
