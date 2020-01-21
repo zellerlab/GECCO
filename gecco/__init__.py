29ce2da1f192989cf61163445d75202095015dc0
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-01-21 16:21:09 +0100

    Add project metadata for future PyPI release

--- gecco/__init__.py
+++ gecco/__init__.py
@@ -1,1 +1,8 @@
-
+__author__ = "Jonas Simon Fleck"
+__license__ = "GPLv3"
+__version__ = (
+    __import__("pkg_resources")
+    .resource_string(__name__, "_version.txt")
+    .strip()
+    .decode("ascii")
+)
