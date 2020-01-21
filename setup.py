67168781b3c8e9101d4d0c48e2f762b8de48a1ff
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-01-21 18:13:27 +0100

    Add custom setuptools commands to download PFam on install

--- setup.py
+++ setup.py
@@ -1,176 +1,109 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import configparser
+import hashlib
+import io
 import os
 import sys
-import time
-import subprocess
 import urllib.request
-from setuptools import setup, find_packages
 
-NAME = "GECCO"
-DESCRIPTION = "GEne Cluster prediction with COnditional random fields."
-URL = "https://git.embl.de/fleck/GECCO.git"
-EMAIL = "jonas.simon.fleck@gmail.com"
-AUTHOR = "Jonas Simon Fleck"
-REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.2.0"
-LICENSE = "GPL-3.0"
-
-# Python package requirements
-REQUIRED = [
-    "numpy >= 1.14.0",
-    "scipy >= 1.1.0",
-    "sklearn-crfsuite >= 0.3.6",
-    "scikit-learn >= 0.19.1",
-    "joblib >= 0.11",
-    "pandas >= 0.22.0",
-    "biopython >= 1.72"
-]
-
-# Path to Pfam Database
-PFAM_DB = "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam31.0/Pfam-A.hmm.gz"
-
-# Read README.md for long description
-HERE = os.path.abspath(os.path.dirname(sys.argv[0]))
-
-try:
-    with open(os.path.join(HERE, "README.md"), "rt") as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# FUNC
-def safe_extract(file):
-    extract_cmd = "gzip -df " + file
-    try:
-        FNULL = open(os.devnull, 'w')
-        process = subprocess.Popen(extract_cmd.split(),stderr=FNULL,stdout=FNULL)
-        output, error = process.communicate()
-    except:
-        sys.stdout.write("Error: failed to extract files\n")
-        sys.exit(1)
-    if process.returncode:
-        sys.stdout.write("Error: failed to extract files\n")
-        sys.exit(1)
-    else:
-        sys.stdout.write("Done.\n")
-
-def reporthook(count, block_size, total_size):
-    """Reports download progress"""
-    global start_time
-    if count == 0:
-        start_time = time.time()
-        return
-    duration = time.time() - start_time
-    progress_size = int(count * block_size)
-    speed = int(progress_size / (1024 * duration))
-    percent = int(count * block_size * 100 / total_size)
-    prog = int(50 * percent / 100)
-    sys.stdout.write(
-        "\rPfam-A.hmm.gz: {0}{1} | {2}%, {3} MB, {4} KB/s, {5} seconds passed".format(
-        "#" * prog, " " * (50 - prog),
-        percent, int(progress_size / (1024 * 1024)), speed, int(duration)))
-    sys.stdout.flush()
+import setuptools
+from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.sdist import sdist as _sdist
 
 
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
+class Resource(setuptools.Extension):
+    """A phony `Extension` that will download resource when "compiled".
     """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " (y/n) "
-    elif default == "yes":
-        prompt = " ([y]/n) "
-    elif default == "no":
-        prompt = " (y/[n]) "
-    else:
-        raise ValueError("invalid default answer: '{0}'".format(default))
 
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
+    def __init__(self, url, name):
+        self.url = url
+        self.target = os.path.join(*name.split('.'))
+        setuptools.Extension.__init__(self, name, [f"{self.target}.hmm.gz.md5"])
 
-# MAIN
+
+class build_ext(_build_ext):
+    """An extension to the `build_ext` command that can download resources.
+    """
+
+    def _download(self, src_url, dst_path):
+        import tqdm
+
+        hasher = hashlib.md5()
+        with urllib.request.urlopen(src_url) as src:
+            with open(dst_path, "wb") as dst:
+
+                content_length = int(src.headers['Content-Length'])
+                read = lambda: src.read(io.DEFAULT_BUFFER_SIZE)
+
+                pbar = tqdm.tqdm(
+                    total=content_length,
+                    unit='B',
+                    unit_scale=True,
+                    unit_divisor=1024,
+                    leave=False,
+                    file=sys.stdout,
+                    miniters=-1,
+                )
+
+                for chunk in iter(read, b''):
+                    hasher.update(chunk)
+                    pbar.update(dst.write(chunk))
+
+                    if not sys.stdout.isatty():
+                        pbar.refresh()
+                        sys.stdout.write('\n')
+
+        pbar.close()
+        return hasher.hexdigest()
+
+    def get_ext_filename(self, ext_name):
+        basepath = os.path.join(*ext_name.split("."))
+        return f'{basepath}.hmm.gz'
+
+    def build_extension(self, ext):
+        # Get the destination path where `setuptools` wants the file to be
+        dst_file = self.get_ext_fullpath(ext.name)
+        dst_dir = os.path.dirname(dst_file)
+        self.mkpath(dst_dir)
+
+        # Download the file to the requested location
+        self.announce(f"downloading {ext.url}", 2)
+        actual_md5 = self._download(ext.url, dst_file)
+
+        #
+        self.announce(f"checking MD5 checksum of {dst_file}")
+        with open(ext.sources[0]) as f:
+            expected_md5 = f.readline().strip()
+
+        #
+        assert actual_md5 == expected_md5
+
+
+class sdist(_sdist):
+    """An extension to the `sdist` command that generates a `pyproject.toml`.
+    """
+
+    def run(self):
+        # build `pyproject.toml` from `setup.cfg`
+        c = configparser.ConfigParser()
+        c.add_section("build-system")
+        c.set("build-system", "requires", str(self.distribution.setup_requires))
+        c.set("build-system", 'build-backend', '"setuptools.build_meta"')
+        with open("pyproject.toml", "w") as pyproject:
+            c.write(pyproject)
+
+        # run the rest of the packaging
+        _sdist.run(self)
+
+
 if __name__ == "__main__":
-
-    sys.stdout.write(" ------------------------------------------------------\n")
-    sys.stdout.write("|                     Greetings!                       |\n")
-    sys.stdout.write("|            Welcome to the GECCO installer            |\n")
-    sys.stdout.write(" ------------------------------------------------------\n")
-
-    default_dir = os.path.join(HERE, "data/pfam/")
-
-    sys.stdout.write("Downloading the Pfam database (~1.2Gb).\n")
-    default = query_yes_no("Do you want to download the database in the current program directory ({0})?".format(default_dir))
-
-    if default:
-        if not os.path.exists(default_dir):
-            os.makedirs(default_dir)
-        db_dir = default_dir
-    else:
-        db_dir = input("Please specify the directory where you wish to download the Pfam database:\n> ")
-        if not os.path.exists(db_dir):
-            os.makedirs(db_dir)
-
-    db_path = os.path.join(os.path.realpath(db_dir), "Pfam-A.hmm.gz")
-
-    force = False
-    if os.path.exists(db_path[:-3]):
-        force = query_yes_no("There is already a Pfam database in that directory. Do you want to overwrite it with the current download?", default="no")
-
-    if force or not os.path.exists(db_path[:-3]):
-        urllib.request.urlretrieve(PFAM_DB, db_path, reporthook)
-        sys.stdout.write("\nDone.\n")
-        sys.stdout.write("Decompressing the Pfam database.\n")
-        safe_extract(db_path)
-
-    config_path = os.path.join(HERE, "data/db_config.txt")
-    with open(config_path, "wt") as f:
-        f.write(db_path[:-3] + "\n")
-
-    sys.stdout.write("Setting up Python dependencies\n\n")
-
-    try:
-        setup(
-            name = NAME,
-            version = VERSION,
-            url = URL,
-            author = AUTHOR,
-            license = LICENSE,
-            author_email = EMAIL,
-            long_description = long_description,
-            description = DESCRIPTION,
-            python_requires = REQUIRES_PYTHON,
-            packages = find_packages(),
-            install_requires = REQUIRED,
-        )
-        sys.stdout.write("Done.\n")
-
-    except:
-        sys.stdout.write("It seems like the python dependencies could not be set up correctly. You might not have enough permissions to write to the PYTHONPATH.\nIf you are on a remote server, you can try re-running the installation from within a conda environment with pip installed.\nOtherwise, you might have to supply the following python dependencies manually:\n\n{0}\n\n".format(
-        "\n".join(REQUIRED)))
-
-    to_path = query_yes_no("Do you want to add gecco.py to your PATH?")
-    if to_path:
-        os.system("chmod +x " + os.path.join(HERE, "gecco.py"))
-        os.system("echo '# GECCO' >> ~/.bashrc")
-        os.system("echo 'export PATH=\"{0}:$PATH\"' >> ~/.bashrc".format(HERE))
-        os.system("source ~/.bashrc")
-
-    sys.stdout.write("\nCongratulations! GECCO has been successfully installed.\n\n")
+    setuptools.setup(
+        cmdclass={"build_ext": build_ext},
+        ext_modules=[
+            Resource(
+                "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam31.0/Pfam-A.hmm.gz",
+                "gecco.data.hmms.Pfam-A"
+            )],
+    )
