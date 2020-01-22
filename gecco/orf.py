d0919ca59f1667b6852804391a0216e77650c69c
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-01-22 15:37:27 +0100

    Add type annotations to `gecco.orf`

--- gecco/orf.py
+++ gecco/orf.py
@@ -1,23 +1,45 @@
-import os
 import errno
+import os
 import subprocess
+import typing
+from typing import Optional, List
+
 
 class ORFFinder(object):
-    """Finds ORFs given a FASTA file and writes the results to out_dir"""
+    """Finds ORFs given a FASTA file containing a genome.
 
-    def __init__(self, fasta, out_dir, method="prodigal",
-            out_formats=None,
-            other_args=None):
+    Arguments:
+        fasta (str): The path to the FASTA file containing the input genome.
+        out_dir (str): The path to the directory in which to write the
+            results files.
+        method (str): The method to use to extract the ORFs. Only *prodigal*
+            is supported at the moment. [default: prodigal]
+        out_formats (list, optional): A list of output formats to produce in
+            supplement of the FASTA file containing the proteins.
+            [default: ["genes", "coords"]]
+        other_args (list, optional): A list of arbitrary arguments to pass to
+            the invoked binary. [default: []].
+
+    """
+
+    def __init__(
+            self,
+            fasta: str,
+            out_dir: str,
+            method: str ="prodigal",
+            out_formats: Optional[List[str]] = None,
+            other_args: Optional[List[str]] = None
+    ) -> None:
         self.fasta = fasta
-        self.base = ".".join(os.path.basename(fasta).split(".")[:-1])
+        self.base, _ = os.path.splitext(os.path.basename(fasta))
         self.out_dir = out_dir
         self.method = method
         self._check_method()
 
         self.out_formats =  ["genes", "coords"] if out_formats is None else out_formats
-        self.other_args = [] if other_args is None else other_args
+        self.other_args: List[str] = [] if other_args is None else other_args
 
-    def run(self):
+    def run(self) -> str:
         """Find ORFs"""
         cmd = self._make_commandline()
         log_out = os.path.join(self.out_dir, self.base + f".{self.method}.log")
@@ -25,17 +47,17 @@ class ORFFinder(object):
             subprocess.run(cmd, stdout=out, stderr=out)
         return self.main_out
 
-    def _check_method(self):
+    def _check_method(self) -> None:
         """Checks wether chosen method is available. Raises error if not."""
         try:
-            devnull = open(os.devnull)
+            devnull = subprocess.DEVNULL
             subprocess.run([self.method], stdout=devnull, stderr=devnull)
         except OSError as err:
             if err.errno == errno.ENOENT:
                 raise RuntimeError(f"{self.method} does not seem to be installed. Please install it and re-run GECCO.") from None
             raise
 
-    def _make_commandline(self):
+    def _make_commandline(self) -> List[str]:
         """Makes commandline for subprocess"""
         cmd = [self.method, "-i", self.fasta]
         self.main_out = os.path.join(self.out_dir, self.base + ".proteins.faa")
