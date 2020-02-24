6d91029ceb6b5ca18ee38f8905bdf1ce99cb0480
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-02-24 14:38:25 +0100

    Simplify `gecco.bgc.BGC` constructor and add more API documentation

--- gecco/bgc.py
+++ gecco/bgc.py
@@ -5,32 +5,63 @@ from typing import List, Optional
 import numpy as np
 from gecco.preprocessing import flatten
 
+
 class Protein(object):
     """Definition of a Protein
     """
 
     def __init__(
-            self,
-            seq_id: str,
-            start: int,
-            end: int,
-            name,
-            p: float = 0.0,
-            domains = None,
-            weights: Optional[List[float]] = None
+        self,
+        seq_id: str,
+        start: int,
+        end: int,
+        name: str,
+        p: float = 0.0,
+        domains: Optional[List[str]] = None,
+        weights: Optional[List[float]] = None,
     ) -> None:
+        """Create a new `Protein` object.
+
+        For practical reasons the strandedness of the protein is not taken
+        into account, i.e. the ``start`` attribute will be the lowest of the
+        two indices delimiting the protein.
+
+        Arguments:
+            seq_id (str): The identifier of the sequence containing the
+                protein.
+            start (int): The location where the protein starts.
+            end (int): The location where the protein ends.
+            name (str): The identifier of the protein itself.
+            p (float): The probability with which the protein was predicted.
+            domains (iterable of `str`): The domains of the protein, if any
+                were predicted.
+            weights (iterable of `flaot`): The prediction weights associated
+                with each domain of the protein.
+
+        Raises:
+            ValueError: when `domains` and `weights` are not the same size.
+        """
         self.seq_id = seq_id
         self.start = min(start, end)
         self.end = max(start, end)
-        self.name = np.array(name)
-        self.domains = np.array([] if domains is None else domains)
+        self.name = name
+        self.p: np.ndarray = np.array(p)
+
+        self.domains = np.array([] if domains is None else list(domains))
         if weights is not None:
-            self.weights: np.ndarray = np.array(weights)
+            self.weights: np.ndarray = np.array(list(weights))
         else:
             self.weights = np.ones(len(domains))
-        self.probs: np.ndarray = np.array(p)
+        if len(self.weights) != len(self.domains):
+            raise ValueError("length of `domains` and `weights` differs")
 
     def is_potential_cluster(self, thresh: float = 0.3) -> bool:
+        """Check whether or not this protein is a potential cluster.
+
+        Arguments:
+            thresh (float): The probability threshold above which a protein is
+                considered a potential cluster. Defaults to `0.3`.
+        """
         return self.probs.mean() > thresh
 
 
@@ -39,44 +70,40 @@ class BGC(object):
     """
 
     def __init__(
-            self,
-            proteins: Optional[List[Protein]] = None,
-            name: Optional[str] = None,
-            bgc_type: Optional[str] = None,
-            type_prob: Optional[float] = None,
-            seq_id: Optional [str] = None,
-            start: Optional[int] = None,
-            end: Optional[int] = None,
-            domains=None,
-            weights: Optional[List[float]] = None
+        self,
+        proteins: List[Protein],
+        name: Optional[str] = None,
+        bgc_type: Optional[str] = None,
+        type_prob: Optional[float] = None,
     ) -> None:
+        """Create a `BGC` from a list of `Protein` objects.
+
+        Arguments:
+            proteins (`list` of `Protein`): The proteins contained in the
+                current BGC.
+            name (`str`, optional): A name to use to identify the BGC. If none
+                is given, then the sequence id of any `Protein` will be used.
+            bgc_type (`str`, optional): The BGC type, if it is known.
+            type_prob (`float`, optional): The probability with which the BGC
+                type was predicted, if any.
+
+        """
         self.proteins = proteins
-        if proteins:
-            self.seq_id = self.proteins[0].seq_id
-            self.prot_ids = np.array([p.name for p in proteins])
-            self.start = min(p.start for p in proteins)
-            self.end = max(p.end for p in proteins)
-            self.domains = np.array([p.domains for p in proteins])
-            self.weights = np.array([p.weights for p in proteins])
-            self.probs = np.array([p.probs for p in proteins])
-        else:
-            self.seq_id = seq_id
-            self.start = start
-            self.end = end
-            self.domains = domains
-            self.weights = weights
-        self.name = name if name else seq_id
-        self.type = bgc_type
+        self.seq_id = self.proteins[0].seq_id
+        self.prot_ids = np.array([p.name for p in proteins])
+        self.start = min(p.start for p in proteins)
+        self.end = max(p.end for p in proteins)
+        self.domains = np.array([p.domains for p in proteins])
+        self.weights = np.array([p.weights for p in proteins])
+        self.probs = np.array([p.probs for p in proteins])
+        self.name = name if name else self.seq_id
+        self.bgc_type = bgc_type
         self.type_prob = type_prob
 
-    def is_valid(self, criterion: str ="antismash", thresh: float =0.6) -> bool:
+    def is_valid(self, criterion: str = "antismash", thresh: float = 0.6) -> bool:
         if criterion == "antismash":
             # These are the default options only
-            return self._antismash_check(
-                n_biopfams = 5,
-                p_thresh = 0.6,
-                n_cds = 5
-            )
+            return self._antismash_check(n_biopfams=5, p_thresh=0.6, n_cds=5)
         elif criterion == "gecco":
             return self._gecco_check()
         else:
@@ -92,19 +119,19 @@ class BGC(object):
                 self.end,
                 probs.mean(),
                 probs.max(),
-                self.type,
-                self.type_prob
+                self.bgc_type,
+                self.type_prob,
             ]
             if long:
-                row.append(",".join(np.hstack(self.prot_ids))) # prots
-                row.append(",".join(np.hstack(self.domains))) # pfam
+                row.append(",".join(np.hstack(self.prot_ids)))  # prots
+                row.append(",".join(np.hstack(self.domains)))  # pfam
         else:
-            row = [self.seq_id, self.name, self.type]
+            row = [self.seq_id, self.name, self.bgc_type]
         row = map(str, row)
         csv.writer(handle, dialect="excel-tab").writerow(row)
 
     def domain_composition(self, all_possible=None):
-        """Computes weighted domain composition with respect to all_possible.
+        """Computes weighted domain composition with respect to ``all_possible``.
         """
         doms = np.hstack(self.domains)
         w = np.hstack(self.weights)
@@ -118,7 +145,7 @@ class BGC(object):
         comp_arr = comp_arr / comp_arr.sum()
         return np.nan_to_num(comp_arr, copy=False)
 
-    def domain_counts(self, all_possible=None):
+    def domain_counts(self, all_possible=None) -> np.ndarray:
         """Computes domain counts with respect to all_possible.
         """
         doms = list(np.hstack(self.domains))
@@ -134,34 +161,38 @@ class BGC(object):
         cluster
         """
 
+        # fmt: off
         bio_pfams = {
-            "PF00109", "PF02801", "PF08659", "PF00378", "PF08541",
-            "PF08545", "PF02803", "PF00108", "PF02706", "PF03364", "PF08990", "PF00501",
-            "PF00668", "PF08415", "PF00975", "PF03061", "PF00432", "PF00494", "PF03936",
-            "PF01397", "PF00432", "PF04275", "PF00348", "PF02401", "PF04551", "PF00368",
-            "PF00534", "PF00535", "PF02922", "PF01041","PF00128", "PF00908","PF02719",
-            "PF04321", "PF01943", "PF02806", "PF02350", "PF02397", "PF04932","PF01075",
-            "PF00953","PF01050", "PF03033", "PF01501", "PF05159", "PF04101", "PF02563",
-            "PF08437", "PF02585", "PF01721", "PF02052", "PF02674","PF03515", "PF04369",
-            "PF08109", "PF08129", "PF09221", "PF09683", "PF10439", "PF11420", "PF11632",
-            "PF11758", "PF12173","PF04738", "PF04737", "PF04604", "PF05147", "PF08109",
-            "PF08129", "PF08130", "PF00155", "PF00202", "PF00702", "PF06339","PF04183",
-            "PF10331", "PF03756", "PF00106", "PF01370", "PF00107", "PF08240", "PF00441",
-            "PF02770", "PF02771", "PF08028","PF01408", "PF02894", "PF00984", "PF00725",
-            "PF03720", "PF03721", "PF07993", "PF02737", "PF00903", "PF00037", "PF04055",
-            "PF00171", "PF00067", "PF01266", "PF01118", "PF02668", "PF00248", "PF01494",
-            "PF01593", "PF03992", "PF00355", "PF01243","PF00384", "PF01488", "PF00857",
-            "PF04879", "PF08241", "PF08242", "PF00698", "PF00483", "PF00561", "PF00583",
-            "PF01636","PF01039", "PF00288", "PF00289", "PF02786", "PF01757", "PF02785",
-            "PF02409", "PF01553", "PF02348", "PF00891", "PF01596","PF04820", "PF02522",
-            "PF08484", "PF08421"
+            "PF00109", "PF02801", "PF08659", "PF00378", "PF08541", "PF08545",
+            "PF02803", "PF00108", "PF02706", "PF03364", "PF08990", "PF00501",
+            "PF00668", "PF08415", "PF00975", "PF03061", "PF00432", "PF00494",
+            "PF03936", "PF01397", "PF00432", "PF04275", "PF00348", "PF02401",
+            "PF04551", "PF00368", "PF00534", "PF00535", "PF02922", "PF01041",
+            "PF00128", "PF00908", "PF02719", "PF04321", "PF01943", "PF02806",
+            "PF02350", "PF02397", "PF04932", "PF01075", "PF00953", "PF01050",
+            "PF03033", "PF01501", "PF05159", "PF04101", "PF02563", "PF08437",
+            "PF02585", "PF01721", "PF02052", "PF02674", "PF03515", "PF04369",
+            "PF08109", "PF08129", "PF09221", "PF09683", "PF10439", "PF11420",
+            "PF11632", "PF11758", "PF12173", "PF04738", "PF04737", "PF04604",
+            "PF05147", "PF08109", "PF08129", "PF08130", "PF00155", "PF00202",
+            "PF00702", "PF06339", "PF04183", "PF10331", "PF03756", "PF00106",
+            "PF01370", "PF00107", "PF08240", "PF00441", "PF02770", "PF02771",
+            "PF08028", "PF01408", "PF02894", "PF00984", "PF00725", "PF03720",
+            "PF03721", "PF07993", "PF02737", "PF00903", "PF00037", "PF04055",
+            "PF00171", "PF00067", "PF01266", "PF01118", "PF02668", "PF00248",
+            "PF01494", "PF01593", "PF03992", "PF00355", "PF01243", "PF00384",
+            "PF01488", "PF00857", "PF04879", "PF08241", "PF08242", "PF00698",
+            "PF00483", "PF00561", "PF00583", "PF01636", "PF01039", "PF00288",
+            "PF00289", "PF02786", "PF01757", "PF02785", "PF02409", "PF01553",
+            "PF02348", "PF00891", "PF01596", "PF04820", "PF02522", "PF08484",
+            "PF08421",
         }
 
         bio_crit = len(set(np.hstack(self.domains)) & bio_pfams) >= n_biopfams
         p_crit = np.mean([p.mean() for p in self.probs]) >= p_thresh
         cds_crit = len(np.hstack(self.prot_ids)) >= n_cds
 
-        return (bio_crit and p_crit and cds_crit)
+        return bio_crit and p_crit and cds_crit
 
     def _gecco_check(self) -> bool:
         return True
