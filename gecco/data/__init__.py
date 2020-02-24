585aea9a3e7f6ba27fe63fdaee672801d3f79771
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-02-24 13:20:26 +0100

    Make `gecco.data.realpath` throw exception when file cannot be located

--- gecco/data/__init__.py
+++ gecco/data/__init__.py
@@ -1,12 +1,23 @@
 import io
+import os
 import pkg_resources
 
 
 def realpath(local_path):
-    return pkg_resources.resource_filename(__name__, local_path)
+    """Get the system path to a data file in the `gecco.data` module.
+
+    Raises:
+        FileNotFoundError: when the data file cannot be located.
+    """
+    path = pkg_resources.resource_filename(__name__, local_path)
+    if not os.path.exists(path):
+        raise FileNotFoundError(path)
+    return path
 
 
 def open(local_path, mode="r"):
+    """Get a file handle to a data file in the `gecco.data` module.
+    """
     stream = pkg_resources.resource_stream(__name__, local_path)
     if mode == "r":
         stream = io.TextIOWrapper(stream)
