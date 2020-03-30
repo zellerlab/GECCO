2f048bdd209e9048b6c8b9548de6166c413e010e
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-03-30 15:50:29 +0200

    Start refactoring cross-validation interface

--- gecco/cli/commands/cv.py
+++ gecco/cli/commands/cv.py
@@ -8,7 +8,7 @@ from typing import List
 import pandas
 
 from ._base import Command
-from ..crf import ClusterCRF
+from ...crf import ClusterCRF
 
 if typing.TYPE_CHECKING:
     from pandas import DataFrame
@@ -20,10 +20,10 @@ class Cv(Command):
     doc = f"""
     gecco cv  - {summary}
 
-    Usage
+    Usage:
         gecco cv (-h | --help)
-        gecco train kfold -i <data>  [-w <col>]... [-f <col>]... [options]
-        gecco train loto  -i <data>  [-w <col>]... [-f <col>]... [options]
+        gecco cv kfold -i <data>  [-w <col>]... [-f <col>]... [options]
+        gecco cv loto  -i <data>  [-w <col>]... [-f <col>]... [options]
 
     Arguments:
         -i <data>, --input <data>       a domain annotation table with regions
@@ -54,6 +54,7 @@ class Cv(Command):
                                         features overlap. [default: 2]
         --splits <N>                    number of folds for cross-validation
                                         (if running `kfold`). [default: 10]
+        --shuffle                       enable shuffling of stratified rows.
 
     Parameters - Column Names:
         -y <col>, --y-col <col>         column with class label. [default: BGC]
@@ -72,6 +73,11 @@ class Cv(Command):
     """
 
     def _check(self) -> typing.Optional[int]:
+        retcode = super()._check()
+        if retcode is not None:
+            return retcode
+
+
         # Check the input exists
         input_ = self.args["--input"]
         if not os.path.exists(input_):
@@ -88,24 +94,13 @@ class Cv(Command):
         if self.args["--truncate"] is not None:
             self.args["--truncate"] = int(self.args["--truncate"])
         self.args["--overlap"] = int(self.args["--overlap"])
-        self.args["--min-orfs"] = int(self.args["--min-orfs"])
         self.args["--c1"] = float(self.args["--c1"])
         self.args["--c2"] = float(self.args["--c2"])
         self.args["--splits"] = int(self.args["--splits"])
-        self.args["--neighbors"] = int(self.args["--neighbors"])
         self.args["--e-filter"] = e_filter = float(self.args["--e-filter"])
         if e_filter < 0 or e_filter > 1:
             self.logger.error("Invalid value for `--e-filter`: {}", e_filter)
             return 1
-
-        # Use default threshold value dependeing on postprocessing method
-        if self.args["--threshold"] is None:
-            if self.args["--postproc"] == "gecco":
-                self.args["--threshold"] = 0.4
-            elif self.args["--postproc"] == "antismash":
-                self.args["--threshold"] = 0.6
-        else:
-            self.args["--threshold"] = float(self.args["--threshold"])
 
         # Check the `--jobs`flag
         self.args["--jobs"] = jobs = int(self.args["--jobs"])
@@ -120,17 +115,17 @@ class Cv(Command):
         data_tbl = pandas.read_csv(self.args["--input"], sep="\t", encoding="utf-8")
         self.logger.debug("Filtering results with e-value under {}", self.args["--e-filter"])
         data_tbl = data_tbl[data_tbl["i_Evalue"] < self.args["--e-filter"]]
-        self.logger.debug("Grouping columns by splitting column {!r}", self.args["--split-col"])
+        self.logger.debug("Splitting input by column {!r}", self.args["--split-col"])
         data: List['DataFrame'] = [s for _, s in data_tbl.groupby(self.args["--split-col"])]
 
         if self.args["--shuffle"]:
             self.logger.debug("Shuffling rows")
-            random.shuffle(data_tbl)
+            random.shuffle(data)
 
         # --- CROSS VALIDATION -----------------------------------------------
         crf = ClusterCRF(
             Y_col = self.args["--y-col"],
-            feature_cols = self.args["--feature-col"],
+            feature_cols = self.args["--feature-cols"],
             weight_cols = self.args["--weight-cols"],
             feature_type = self.args["--feature-type"],
             overlap = self.args["--overlap"],
@@ -141,28 +136,20 @@ class Cv(Command):
 
         if self.args["loto"]:
             cv_type = "loto"
-            cross_validate = functools.partial(
-                crf.loto_cv,
-                type_col=self.args["--strat-col"],
-                threads=self.args["--jobs"],
-                trunc=self.args["--trunc"]
-            )
+            cross_validate = crf.loto_cv
         elif self.args["kfold"]:
             cv_type = "kfold"
-            cross_validate = functools.partial(
-                crf.cv,
-                k=self.args["--splits"],
-                strat_col=self.args["--strat-col"],
-                threads=self.args["--jobs"],
-                trunc=self.args["--trunc"]
-            )
-        else:
-            raise ValueError("unknown cross-validation method")
+            cross_validate = functools.partial(crf.cv, k=self.args["--splits"])
 
-        self.args.info("Starting cross-validation")
-        results = pandas.concat(cross_validate(data_tbl))
+        self.logger.info("Performing cross-validation")
+        results = pandas.concat(cross_validate(
+            data,
+            strat_col=self.args["--strat-col"],
+            threads=self.args["--jobs"],
+            trunc=self.args["--truncate"]
+        ))
 
-        self.args.info("Formatting results")
+        self.logger.info("Formatting results")
         results["c1"] = self.args["--c1"]
         results["c2"] = self.args["--c2"]
         results["feature_type"] = self.args["--feature-type"]
@@ -191,5 +178,5 @@ class Cv(Command):
         #                     "weight", "truncate", "cv_type", "cv_round", "in_file"] ])
 
         # Write results
-        self.info("Writing output to {!r}", self.args["--output"])
-        results.to_table(self.args["--output"], index=False)
+        self.logger.info("Writing output to {!r}", self.args["--output"])
+        results.to_csv(self.args["--output"], sep="\t", index=False)
