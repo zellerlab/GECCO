7ba62c0f9f9108893c99ec37b93e1d4796ce0ba7
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-08-05 22:50:48 +0200

    Add test to check the `gecco train` method works as expected

--- tests/test_cli/test_train.py
+++ tests/test_cli/test_train.py
@@ -1,6 +1,36 @@
+import io
+import os
 import unittest
+import tempfile
+import shutil
+
+from gecco.cli import main
 from gecco.cli.commands.train import Train
+
 from ._base import TestCommand
+
 
 class TestTrain(TestCommand, unittest.TestCase):
     command_type = Train
+
+    @property
+    def folder(self):
+        return os.path.dirname(os.path.abspath(__file__))
+
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+
+    def test_train_embedding(self):
+        table = os.path.join(self.folder, "mibig-2.0.Pfam-33.0.Tigrfam-15.0.tsv")
+        argv = ["-vv", "--traceback", "train", "-i", table, "-o", self.tmpdir]
+
+        with io.StringIO() as stream:
+            retcode = main(argv, stream=stream)
+            self.assertEqual(retcode, 0, stream.getvalue())
+
+        files = os.listdir(self.tmpdir)
+        self.assertIn("model.pkl", files)
+        self.assertIn("model.pkl.md5", files)
