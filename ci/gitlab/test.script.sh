0d24cf10e267f0fad64648722734e5e6a0fb3263
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-11-12 18:58:30 +0100

    Fix install of wheel built in GitLab-CI test stage

--- ci/gitlab/test.script.sh
+++ ci/gitlab/test.script.sh
@@ -7,6 +7,5 @@ set -e
 
 
 python setup.py bdist_wheel
-WHEEL=$(python setup.py --name)-$(python setup.py --version)-py2.py3-none-any.whl
-pip install -U "dist/$WHEEL[train]"
+python -m pip install --no-index --find-links=dist gecco[train]
 python -m coverage run -p -m unittest discover -vv
