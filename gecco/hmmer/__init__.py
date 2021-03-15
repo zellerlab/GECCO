e6ec651a844bc76cb51fc26792ce953bb34eb60e
Martin Larralde <martin.larralde@embl.de>
2021-03-15 15:52:43 +0100

    Make `hmmsearch` step process genes grouped by contigs

--- gecco/hmmer/__init__.py
+++ gecco/hmmer/__init__.py
@@ -7,6 +7,7 @@ import contextlib
 import csv
 import errno
 import glob
+import itertools
 import os
 import re
 import subprocess
@@ -18,10 +19,11 @@ import pkg_resources
 import pyhmmer
 from Bio import SeqIO
 
+from ._worker import hmmsearch
 from .._meta import requires
-from .._base import BinaryRunner
 from ..model import Gene, Domain
 from ..interpro import InterPro
+
 
 if typing.TYPE_CHECKING:
     from Bio.SeqRecord import SeqRecord
@@ -30,55 +32,6 @@ if typing.TYPE_CHECKING:
 
 
 __all__ = ["DomainRow", "HMM", "HMMER", "PyHMMER", "embedded_hmms"]
-
-
-class DomainRow(typing.NamedTuple):
-    """A single row in a domain table created by ``hmmsearch``.
-
-    See Also:
-        The description of each field in page 48 of the `HMMER manual
-        <http://eddylab.org/software/hmmer3/3.1b2/Userguide.pdf>`_.
-
-    """
-
-    target_name: str
-    target_accession: Optional[str]
-    target_length: int
-    query_name: str
-    query_accession: Optional[str]
-    query_length: int
-    evalue: float
-    score: float
-    bias: float
-    domain_number: int
-    domain_total: int
-    c_evalue: float
-    i_evalue: float
-    domain_score: float
-    domain_bias: float
-    hmm_from: int
-    hmm_to: int
-    ali_from: int
-    ali_to: int
-    env_from: int
-    env_to: int
-    post: float
-    description: Optional[str]
-
-    @classmethod
-    def from_line(cls: Type["_T"], row: str) -> "_T":
-        """Extract a `DomainRow` from a single domain table line.
-        """
-        line = list(filter(None, row.split(" ")))
-        values = {}
-        for i, (field, ty) in enumerate(cls.__annotations__.items()):
-            if isinstance(ty, type):
-                values[field] = ty(line[i])
-            elif field == "target_accession" or field == "query_accession":
-                values[field] = None if line[i] == "-" else line[i]
-            elif field == "description":
-                values[field] = " ".join(line[i:]) if line[i:] else None
-        return cls(**values)
 
 
 class HMM(typing.NamedTuple):
@@ -134,68 +87,8 @@ class DomainAnnotator(metaclass=abc.ABCMeta):
         return NotImplemented
 
 
-class HMMER(DomainAnnotator):
-    """A wrapper for HMMER that uses the ``hmmsearch`` binary.
-    """
-
-    BINARY = "hmmsearch"
-
-    def __init__(self, hmm: HMM, cpus: Optional[int] = None) -> None:
-        DomainAnnotator.__init__(self, hmm, cpus)
-        BinaryRunner.__init__(self)
-
-    def run(self, genes: Iterable[Gene]) -> List[Gene]:
-        # collect genes and build an index of genes by protein id
-        gene_index = collections.OrderedDict([(gene.id, gene) for gene in genes])
-
-        # create a temporary file to write the input and output to
-        seqs_tmp = tempfile.NamedTemporaryFile(prefix="hmmer", suffix=".faa")
-        doms_tmp = tempfile.NamedTemporaryFile(prefix="hmmer", suffix=".dom", mode="rt")
-
-        # write protein sequences
-        protein_records = (g.protein.to_seq_record() for g in gene_index.values())
-        SeqIO.write(protein_records, seqs_tmp.name, "fasta")
-
-        # Prepare the command line arguments
-        cmd = ["hmmsearch", "--noali", "--domtblout", doms_tmp.name]
-        if self.cpus is not None:
-            cmd.extend(["--cpu", str(self.cpus)])
-        cmd.extend([self.hmm.path, seqs_tmp.name])
-
-        # Run HMMER
-        subprocess.run(cmd, stdout=subprocess.DEVNULL).check_returncode()
-
-        # Load InterPro metadata for the annotation
-        interpro = InterPro.load()
-
-        # Read the domain table
-        lines = filter(lambda line: not line.startswith("#"), doms_tmp)
-        rows = map(DomainRow.from_line, lines)
-
-        # update protein domains
-        for row in rows:
-            # extract domain from the domain table row
-            accession = self.hmm.relabel(row.query_accession or row.query_name)
-            entry = interpro.by_accession.get(accession)
-            # add additional qualifiers with available metadata
-            qualifiers: Dict[str, List[str]] = {
-                "inference": ["protein motif"],
-                "note": ["e-value: {}".format(row.i_evalue)],
-                "db_xref": ["{}:{}".format(self.hmm.id.upper(), accession)],
-                "function": [] if entry is None else [entry.name]
-            }
-            if entry is not None and entry.integrated is not None:
-                qualifiers["db_xref"].append("InterPro:{}".format(entry.integrated))
-            # add the domain to the protein domains of the right gene
-            domain = Domain(accession, row.env_from, row.env_to, self.hmm.id, row.i_evalue, None, qualifiers)
-            gene_index[row.target_name].protein.domains.append(domain)
-
-        # return the updated list of genes that was given in argument
-        return list(gene_index.values())
-
-
 class PyHMMER(DomainAnnotator):
-    """A domain annotator that uses `pyhmmer.hmmer.hmmsearch`.
+    """A domain annotator that uses `pyhmmer`.
     """
 
     def run(
@@ -206,47 +99,54 @@ class PyHMMER(DomainAnnotator):
         # collect genes and build an index of genes by protein id
         gene_index = collections.OrderedDict([(gene.id, gene) for gene in genes])
 
-        # convert to Easel sequences
+        # group genes by contig
+        by_source  = collections.defaultdict(list)
+        for gene in gene_index.values():
+            by_source[gene.source.id].append(gene)
+
+        # convert to Easel sequences, grouped by contig
         esl_abc = pyhmmer.easel.Alphabet.amino()
         esl_sqs = [
-            pyhmmer.easel.TextSequence(
-                name=gene.protein.id.encode(),
-                sequence=str(gene.protein.seq)
-            ).digitize(esl_abc)
-            for gene in gene_index.values()
+            [
+                pyhmmer.easel.TextSequence(
+                    name=gene.protein.id.encode(),
+                    sequence=str(gene.protein.seq)
+                ).digitize(esl_abc)
+                for gene in contig_genes
+            ]
+            for contig_genes in by_source.values()
         ]
 
         # Run HMMER subprocess.run(cmd, stdout=subprocess.DEVNULL).check_returncode()
         with pyhmmer.plan7.HMMFile(self.hmm.path) as hmm_file:
             cpus = 0 if self.cpus is None else self.cpus
-            hmms_hits = pyhmmer.hmmsearch(hmm_file, esl_sqs, cpus=cpus, callback=progress)
+            hmms_hits = hmmsearch(hmm_file, esl_sqs, cpus=cpus, callback=progress)
 
             # Load InterPro metadata for the annotation
             interpro = InterPro.load()
 
             # Transcribe HMMER hits to GECCO model
-            for hits in hmms_hits:
-                for hit in hits:
-                    target_name = hit.name.decode('utf-8')
-                    for domain in hit.domains:
-                        raw_acc = domain.alignment.hmm_accession or domain.alignment.hmm_name
-                        accession = self.hmm.relabel(raw_acc.decode('utf-8'))
-                        entry = interpro.by_accession.get(accession)
+            for hit in itertools.chain.from_iterable(hmms_hits):
+                target_name = hit.name.decode('utf-8')
+                for domain in hit.domains:
+                    raw_acc = domain.alignment.hmm_accession or domain.alignment.hmm_name
+                    accession = self.hmm.relabel(raw_acc.decode('utf-8'))
+                    entry = interpro.by_accession.get(accession)
 
-                        # extract qualifiers
-                        qualifiers: Dict[str, List[str]] = {
-                            "inference": ["protein motif"],
-                            "note": ["e-value: {}".format(domain.i_evalue)],
-                            "db_xref": ["{}:{}".format(self.hmm.id.upper(), accession)],
-                            "function": [] if entry is None else [entry.name]
-                        }
-                        if entry is not None and entry.integrated is not None:
-                            qualifiers["db_xref"].append("InterPro:{}".format(entry.integrated))
+                    # extract qualifiers
+                    qualifiers: Dict[str, List[str]] = {
+                        "inference": ["protein motif"],
+                        "note": ["e-value: {}".format(domain.i_evalue)],
+                        "db_xref": ["{}:{}".format(self.hmm.id.upper(), accession)],
+                        "function": [] if entry is None else [entry.name]
+                    }
+                    if entry is not None and entry.integrated is not None:
+                        qualifiers["db_xref"].append("InterPro:{}".format(entry.integrated))
 
-                        # add the domain to the protein domains of the right gene
-                        assert domain.env_from < domain.env_to
-                        domain = Domain(accession, domain.env_from, domain.env_to, self.hmm.id, domain.i_evalue, None, qualifiers)
-                        gene_index[target_name].protein.domains.append(domain)
+                    # add the domain to the protein domains of the right gene
+                    assert domain.env_from < domain.env_to
+                    domain = Domain(accession, domain.env_from, domain.env_to, self.hmm.id, domain.i_evalue, None, qualifiers)
+                    gene_index[target_name].protein.domains.append(domain)
 
         # return the updated list of genes that was given in argument
         return list(gene_index.values())
