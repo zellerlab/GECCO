8ec91a55c0edb8555072545090489f42e782062b
Martin Larralde <martin.larralde@embl.de>
2021-05-07 16:57:42 +0200

    Write version to `gecco/__init__.py` to avoid importing `pkg_resources`

--- gecco/__init__.py
+++ gecco/__init__.py
@@ -3,9 +3,4 @@
 
 __author__ = "Martin Larralde"
 __license__ = "GPLv3"
-__version__ = (
-    __import__("pkg_resources")
-    .resource_string(__name__, "_version.txt")
-    .strip()
-    .decode("ascii")
-)
+__version__ = "0.6.2"
