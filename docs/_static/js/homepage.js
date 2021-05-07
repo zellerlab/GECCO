35f6529ab7adf9e32f82ff22352b2ce543a7fdf2
Martin Larralde <martin.larralde@embl.de>
2021-05-07 18:31:07 +0200

    Fix formatting of badges in HTML documentation

--- docs/_static/js/homepage.js
+++ docs/_static/js/homepage.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
   (function ($) {
-    if (window.location.href.match("/index.html") !== null) {
-      $("#gecco").children("p").last().html( (index, text) => text.replaceAll("</a> ", "</a>") ).end();
-    }
+    $("#gecco").children("p").last().html( (index, text) => text.replaceAll("</a> ", "</a>") ).end();
   })(window.$jqTheme || window.jQuery);
 })
