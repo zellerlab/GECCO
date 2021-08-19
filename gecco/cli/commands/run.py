8554595e0504bbad7f28cff08b3d94d45a06b3e5
Martin Larralde <martin.larralde@embl.de>
2021-08-19 18:11:37 +0200

    Revert back to model from v0.8.2
    This reverts commit 23c77f2b631a064137b1a890533466bcfb5e470d.

--- gecco/cli/commands/run.py
+++ gecco/cli/commands/run.py
@@ -68,7 +68,7 @@ class Run(Annotate):  # noqa: D101
             -e <e>, --e-filter <e>        the e-value cutoff for protein domains
                                           to be included.
             -p <p>, --p-filter <p>        the p-value cutoff for protein domains
-                                          to be included. [default: 1e-5]
+                                          to be included. [default: 1e-9]
 
         Parameters - Cluster Detection:
             -c, --cds <N>                 the minimum number of coding sequences a
